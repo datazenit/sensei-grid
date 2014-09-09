1b9fca5b4086af86d2fb5865f9430f8367539000
Lauris <lauris@discuss.lv>
2014-09-09 14:02:38 +0300

    Build dist files from latest src changes.

--- dist/sensei-grid.js
+++ dist/sensei-grid.js
@@ -5,53 +5,6 @@
 */
 (function ($) {
 
-    var root = this;
-
-    var Editor = function(grid) {
-        this.grid = grid;
-    };
-    Editor.extend = function (props) {
-        var parent = this;
-        var child;
-
-        child = function(){ return parent.apply(this, arguments); };
-
-        var Surrogate = function(){ this.constructor = child; };
-        Surrogate.prototype = parent.prototype;
-        child.prototype = new Surrogate;
-
-        if (props) {
-            _.extend(child.prototype, props);
-        }
-
-        child.__super__ = parent.prototype;
-
-        return child;
-    };
-    Editor.prototype.getElement = function () {
-        return $(this.editor);
-    };
-    Editor.prototype.initialize = function () {};
-    Editor.prototype.render = function () {};
-    Editor.prototype.show = function () {
-        this.getElement().show();
-    };
-    Editor.prototype.hide = function () {
-        console.log("Editor hide");
-        this.getElement().hide();
-        this.grid.activeEditor.activeCell = null;
-        this.grid.activeEditor = null;
-    };
-    Editor.prototype.getValue = function () {
-        console.warn("Editor.getValue not implemented")
-    };
-    Editor.prototype.setValue = function () {
-        console.warn("Editor.setValue not implemented")
-    };
-
-    // export editor
-    root.Editor = Editor;
-
     $.fn.grid = function (data, columns, options) {
 
         var plugin = this,
@@ -129,8 +82,8 @@
             }
         };
 
-        plugin.registerEditor = function (editor) {
-            var instance = new editor(plugin);
+        plugin.registerEditor = function (Editor) {
+            var instance = new Editor(plugin);
             plugin.editors[instance.name] = instance;
         };
 
@@ -661,6 +614,58 @@
 
 (function ($) {
     var root = this;
+
+    var Editor = function (grid) {
+        this.grid = grid;
+    };
+    Editor.extend = function (props) {
+        var parent = this;
+        var child;
+
+        child = function () {
+            return parent.apply(this, arguments);
+        };
+
+        var Surrogate = function () {
+            this.constructor = child;
+        };
+        Surrogate.prototype = parent.prototype;
+        child.prototype = new Surrogate();
+
+        if (props) {
+            _.extend(child.prototype, props);
+        }
+
+        child.__super__ = parent.prototype;
+
+        return child;
+    };
+    Editor.prototype.getElement = function () {
+        return $(this.editor);
+    };
+    Editor.prototype.initialize = function () {
+    };
+    Editor.prototype.render = function () {
+    };
+    Editor.prototype.show = function () {
+        this.getElement().show();
+    };
+    Editor.prototype.hide = function () {
+        console.log("Editor hide");
+        this.getElement().hide();
+        this.grid.activeEditor.activeCell = null;
+        this.grid.activeEditor = null;
+    };
+    Editor.prototype.getValue = function () {
+        console.warn("Editor.getValue not implemented");
+    };
+    Editor.prototype.setValue = function () {
+        console.warn("Editor.setValue not implemented");
+    };
+
+    // export editor
+    root.Editor = Editor;
+
     root.BasicEditor = Editor.extend({
         types: [],
         name: "BasicEditor",
