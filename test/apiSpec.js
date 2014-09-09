99fd3c9aa693b5ee9b6b42416eb10ef58e349b4f
Lauris <lauris@discuss.lv>
2014-09-09 21:22:11 +0300

    Add tests for remaining public data api methods in apiSpec.

--- test/apiSpec.js
+++ test/apiSpec.js
@@ -111,4 +111,47 @@ describe("sensei-grid api", function () {
             expect(grid.getCellFromRowByIndex($row, 0).get()).toEqual($cell);
         });
     });
+
+    describe("getRowCellsByIndex", function () {
+        it("should return row cells by index", function () {
+            var $cells = $(".sensei-grid-test>table>tbody>tr:first>td").get();
+            expect(grid.getRowCellsByIndex(0).get()).toEqual($cells)
+        });
+    });
+
+    describe("getRowCells", function () {
+        it("should return row cells", function () {
+            var $row = $(".sensei-grid-test>table>tbody>tr:first");
+            expect(grid.getRowCells($row).get()).toEqual($row.find(">td").get());
+        });
+    });
+
+    describe("getCellData", function () {
+        it("should return cell data", function () {
+            var $cells = $(".sensei-grid-test>table>tbody>tr:first>td");
+            expect(grid.getCellData($cells.eq(0))).toBe(data[0]["id"]);
+            expect(grid.getCellData($cells.eq(4))).toBe(data[0]["count"]);
+        });
+        it("should return data with correct type", function () {
+            var $cells = $(".sensei-grid-test>table>tbody>tr:first>td");
+            expect(grid.getCellData($cells.eq(0))).toEqual(jasmine.any(String));
+            expect(grid.getCellData($cells.eq(4))).toEqual(jasmine.any(Number));
+        });
+    });
+
+    describe("cell meta methods", function () {
+        it("getCellType should return data type of cell", function () {
+            var $cells = $(".sensei-grid-test>table>tbody>tr:first>td");
+            expect(grid.getCellType($cells.eq(0))).toEqual("string");
+            expect(grid.getCellType($cells.eq(4))).toEqual("int");
+        });
+        it("getCellColumn should return column name of cell", function () {
+            var $cells = $(".sensei-grid-test>table>tbody>tr:last>td");
+            expect(grid.getCellColumn($cells.eq(0))).toEqual("id");
+            expect(grid.getCellColumn($cells.eq(1))).toEqual("created_at");
+            expect(grid.getCellColumn($cells.eq(2))).toEqual("status");
+            expect(grid.getCellColumn($cells.eq(3))).toEqual("title");
+            expect(grid.getCellColumn($cells.eq(4))).toEqual("count");
+        });
+    });
 });
