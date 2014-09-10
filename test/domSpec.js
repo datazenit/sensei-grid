3624e4a47420b2d1fefdf7dd5d47f033916911e0
Lauris <lauris@discuss.lv>
2014-09-10 15:29:05 +0300

    Fix domSpec: destroy editor in afterEach hook and rename wrapper element to "sensei-grid".

--- test/domSpec.js
+++ test/domSpec.js
@@ -5,7 +5,7 @@ var columns = getColumns(data);
 describe("sensei-grid dom", function () {
 
     var grid;
-    var $el = $('<div class="sensei-grid-test">');
+    var $el = $('<div class="sensei-grid">');
 
     // create dom element before each test
     beforeEach(function () {
@@ -18,29 +18,30 @@ describe("sensei-grid dom", function () {
 
         // render grid
         grid = $el.grid(data, columns);
+        grid.registerEditor(BasicEditor);
         grid.render();
     });
 
     // remove grid wrapper after each test
     afterEach(function () {
-        $(".sensei-grid-test").remove();
+        grid.destroy();
         grid = null;
     });
 
     it("should have a wrapper element", function () {
-        expect($(".sensei-grid-test").length).toBe(1);
+        expect($(".sensei-grid").length).toBe(1);
     });
 
     it("should have a table element and core structure", function () {
-        expect($(".sensei-grid-test>table").length).toBe(1);
-        expect($(".sensei-grid-test>table>thead").length).toBe(1);
-        expect($(".sensei-grid-test>table>tbody").length).toBe(1);
-        expect($(".sensei-grid-test>table>tbody>tr").length).toBe(10);
-        expect($(".sensei-grid-test>table>thead>tr").length).toBe(1);
+        expect($(".sensei-grid>table").length).toBe(1);
+        expect($(".sensei-grid>table>thead").length).toBe(1);
+        expect($(".sensei-grid>table>tbody").length).toBe(1);
+        expect($(".sensei-grid>table>tbody>tr").length).toBe(10);
+        expect($(".sensei-grid>table>thead>tr").length).toBe(1);
     });
 
     it("should render columns", function () {
-        var $ths = $(".sensei-grid-test>table>thead>tr>th");
+        var $ths = $(".sensei-grid>table>thead>tr>th");
         var columnValues = $ths.map(function () {
             return $(this).text();
         }).get();
@@ -50,11 +51,11 @@ describe("sensei-grid dom", function () {
     });
 
     it("should render data", function () {
-        var $firstRow = $(".sensei-grid-test>table>tbody>tr:first");
-        var $lastRow = $(".sensei-grid-test>table>tbody>tr:last");
+        var $firstRow = $(".sensei-grid>table>tbody>tr:first");
+        var $lastRow = $(".sensei-grid>table>tbody>tr:last");
 
         expect($firstRow.find("td:first").text()).toEqual(data[0]["id"]);
-        expect($firstRow.find("td:first").html()).toEqual("<div>"+data[0]["id"]+"</div>");
+        expect($firstRow.find("td:first").html()).toEqual("<div>" + data[0]["id"] + "</div>");
 
         expect($lastRow.find("td:eq(0)").text()).toEqual(data[9]["id"]);
         expect($lastRow.find("td:eq(1)").text()).toEqual(data[9]["created_at"]);
