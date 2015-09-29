c2b3a8468586f05b045494225bf91c2ec7693509
Lauris <lauris@discuss.lv>
2015-09-29 19:50:25 +0300

    Reformat row actions file.

--- src/sensei-row-actions.js
+++ src/sensei-row-actions.js
@@ -27,70 +27,76 @@
 		return child;
 	};
 	RowAction.prototype.initialize = function() {
-    _.bindAll(this, "trigger");
-  };
+		_.bindAll(this, "trigger");
+	};
 	RowAction.prototype.triggerEvent = null;
-	RowAction.prototype.trigger = function(/** $activeCell */) {};
+	RowAction.prototype.trigger = function( /** $activeCell */ ) {};
 	RowAction.prototype.rowElement = function() {
 		throw new Error("Not yet implemented");
 	};
 
 	root.DemoRowAction = RowAction.extend({
 		name: "DemoRowAction",
-    rowElement: function () {
-      return "<button class='btn btn-default btn-xs'>Demo</button>";
-    },
-    triggerEvent: {event: "click", selector: ".btn"},
-    trigger: function (e) {
-      var $activeCell = e.data.$activeCell;
-      console.log("DemoRowAction.trigger", $activeCell);
+		rowElement: function() {
+			return "<button class='btn btn-default btn-xs'>Demo</button>";
+		},
+		triggerEvent: {
+			event: "click",
+			selector: ".btn"
+		},
+		trigger: function(e) {
+			var $activeCell = e.data.$activeCell;
+			console.log("DemoRowAction.trigger", $activeCell);
 
-      // get button from active cell
-      var $btn = $activeCell.find(".btn");
+			// get button from active cell
+			var $btn = $activeCell.find(".btn");
 
-      // simulate some loading action on demo button
-      if (!$btn.prop("disabled")) {
-        $btn.prop("disabled", true)
-          .removeClass("btn-success")
-          .text("Loading...");
+			// simulate some loading action on demo button
+			if (!$btn.prop("disabled")) {
+				$btn.prop("disabled", true)
+					.removeClass("btn-success")
+					.text("Loading...");
 
-        setTimeout(function () {
-          $btn.prop("disabled", false)
-            .addClass("btn-success")
-            .text("Success");
-        }, 1000);
-      }
-    }
+				setTimeout(function() {
+					$btn.prop("disabled", false)
+						.addClass("btn-success")
+						.text("Success");
+				}, 1000);
+			}
+		}
 	});
 
 	root.DeleteRowAction = RowAction.extend({
 		name: "DeleteRowAction",
-    rowElement: function () {
-      return "<button class='btn btn-danger btn-xs'>Delete</button>";
-    },
-    triggerEvent: {event: "click", selector: ".btn"},
-    trigger: function (e) {
-      var $activeCell = e.data.$activeCell;
-      console.log("DeleteRowAction.trigger", $activeCell);
+		rowElement: function() {
+			return "<button class='btn btn-danger btn-xs'>Delete</button>";
+		},
+		triggerEvent: {
+			event: "click",
+			selector: ".btn"
+		},
+		trigger: function(e) {
+			var $activeCell = e.data.$activeCell;
+			console.log("DeleteRowAction.trigger", $activeCell);
 
-      // get button from active cell
-      var $btn = $activeCell.find(".btn");
+			// get button from active cell
+			var $btn = $activeCell.find(".btn");
 
-      // simulate some loading action on demo button
-      if (!$btn.prop("disabled")) {
-        $btn.prop("disabled", true)
-          .text("Deleting...");
+			// simulate some loading action on demo button
+			if (!$btn.prop("disabled")) {
+				$btn.prop("disabled", true)
+					.text("Deleting...");
 
-        var grid = this.grid;
-        var $row = this.grid.getCellRow($activeCell);
+				var grid = this.grid;
+				var $row = this.grid.getCellRow($activeCell);
 
-        setTimeout(function () {
-          $btn.addClass("btn-danger")
-            .text("Deleted");
-          grid.removeRow($row);
-        }, 500);
-      }
-    }
+				setTimeout(function() {
+					$btn.addClass("btn-danger")
+						.text("Deleted");
+					grid.removeRow($row);
+				}, 500);
+			}
+		}
 	});
 
 	// export
